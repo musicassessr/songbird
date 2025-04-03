e12f875125d39b0e27ee2f8bd9aae5a60ad2c366
sebsilas <sebsilas@gmail.com>
2025-04-03 06:29:17 +0000

    Add in extra tests for pretesting

--- R/create_questionnaire_app.R
+++ R/create_questionnaire_app.R
@@ -14,6 +14,7 @@
 #' @param type
 #' @param pre_post
 #' @param img_dir
+#' @param extra_materials
 #'
 #' @returns
 #' @export
@@ -22,7 +23,8 @@
 create_questionnaire_app <- function(force_p_id_from_url = FALSE,
                                      type = c("kids", "parents", "teachers"),
                                      pre_post = c("pre", "post"),
-                                     img_dir = system.file("data-raw/questionnaires/www/img", package = "songbird")) {
+                                     img_dir = system.file("data-raw/questionnaires/www/img", package = "songbird"),
+                                     extra_materials = NULL) {
 
   shiny::addResourcePath(
     prefix = "img", # custom prefix that will be used to reference your directory
@@ -53,80 +55,89 @@ create_questionnaire_app <- function(force_p_id_from_url = FALSE,
     # post test TLs...
   }
 
-  psychTestR::make_test(
-    opt = psychTestR::test_options(
-      title = "SingPause",
-      admin_password = "ilikecheesepie432",
-      researcher_email = "sebsilas@gmail.com",
-      logo = "https://musicassessr.com/assets/songbird_logo.png",
-      logo_height = "70px",
-      logo_width = "190px",
-      logo_position = "left",
-      allow_any_p_id_url = TRUE,
-      force_p_id_from_url = force_p_id_from_url,
-      enable_admin_panel = FALSE,
-      display = psychTestR::display_options(css = c("https://musicassessr.com/assets/css/style_songbird.css",
-                                                    system.file('www/css/musicassessr.css', package = 'musicassessr') ) )
-    ),
+  if(!is.null(extra_materials)) {
+    tl <- psychTestR::join(
+      extra_materials,
+      tl
+    )
+  }
 
-    elts = psychTestR::join(
+  musicassessr::make_musicassessr_test(
 
-                            # Grab URL parameters
+    title = "SingPause",
+    languages = "de",
+    admin_password = "ilikecheesepie432",
+    researcher_email = NULL, # We can't translate the text
+    logo = "https://musicassessr.com/assets/songbird_logo.png",
+    logo_height = "70px",
+    logo_width = "190px",
+    logo_position = "left",
+    allow_any_p_id_url = TRUE,
+    force_p_id_from_url = force_p_id_from_url,
 
-                            psychTestR::reactive_page(function(state, ...) {
+    opt = musicassessr::musicassessr_opt(asynchronous_api_mode = TRUE,
+                                         user_id = 147L, # PRETEST: melody_dev: 147L, melody_prod: 186L;;; POSTTEST: melody_dev: 148L, melody_prod: 187L
+                                         setup_pages = FALSE),
 
-                              url_params <- psychTestR::get_url_params(state)
-                              psychTestR::set_global("singpause_user_id", url_params$user_id, state)
-                              psychTestR::set_global("singpause_username", url_params$username, state)
+    # Grab URL parameters
+    welcome_page = psychTestR::reactive_page(function(state, ...) {
 
-                              psychTestR::one_button_page('Liebe Schülerin, lieber Schüler, im Folgendem werden wir Dir einige kurze Frage stellen. Beantworte sie einfach ganz spontan und so wie Du Dich gerade fühlst. Es gibt kein Richtig oder Falsch. Klicke bitte auf "Weiter" um zu beginnen.', button_text = "Weiter")
+        url_params <- psychTestR::get_url_params(state)
+        psychTestR::set_global("singpause_user_id", url_params$user_id, state)
+        psychTestR::set_global("singpause_username", url_params$username, state)
 
-                            }),
+        psychTestR::one_button_page('Liebe Schülerin, lieber Schüler, im Folgendem werden wir Dir einige kurze Frage stellen. Beantworte sie einfach ganz spontan und so wie Du Dich gerade fühlst. Es gibt kein Richtig oder Falsch. Klicke bitte auf "Weiter" um zu beginnen.', button_text = "Weiter")
 
-                            # Main timeline
-                            tl,
+      }),
 
-                            # Append entry completion
+    elts = function() {
+      psychTestR::join(
 
-                            psychTestR::code_block(function(state, ...) {
+      # Main timeline
+      tl,
 
-                              singpause_user_id <- psychTestR::get_global("singpause_user_id", state)
-                              singpause_id <- psychTestR::get_global("singpause_username", state)
+      # Append entry completion
 
-                              session_info <- psychTestR::get_session_info(state, complete = FALSE)
-                              psychTestR_session_id <- session_info$p_id
+      psychTestR::code_block(function(state, ...) {
 
-                              logging::loginfo("singpause_user_id: %s", singpause_user_id)
-                              logging::loginfo("singpause_id: %s", singpause_id)
-                              logging::loginfo("psychTestR_session_id: %s", psychTestR_session_id)
+        singpause_user_id <- psychTestR::get_global("singpause_user_id", state)
+        singpause_id <- psychTestR::get_global("singpause_username", state)
 
-                              if(length(singpause_user_id) > 0L && length(singpause_id) > 0L && length(psychTestR_session_id) > 0L) {
-                                musicassessrdb::append_singpause_survey_completion_api(user_id = singpause_user_id,
-                                                                                       singpause_id = singpause_id, # Same as username in users
-                                                                                       psychTestR_id = psychTestR_session_id,
-                                                                                       type = paste0(pre_post, "test"))
-                              }
+        session_info <- psychTestR::get_session_info(state, complete = FALSE)
+        psychTestR_session_id <- session_info$p_id
 
-                            }),
+        logging::loginfo("singpause_user_id: %s", singpause_user_id)
+        logging::loginfo("singpause_id: %s", singpause_id)
+        logging::loginfo("psychTestR_session_id: %s", psychTestR_session_id)
 
-                            psychTestR::reactive_page(function(state, ...) {
+        if(length(singpause_user_id) > 0L && length(singpause_id) > 0L && length(psychTestR_session_id) > 0L) {
+          musicassessrdb::append_singpause_survey_completion_api(user_id = singpause_user_id,
+                                                                 singpause_id = singpause_id, # Same as username in users
+                                                                 psychTestR_id = psychTestR_session_id,
+                                                                 type = paste0(pre_post, "test"))
+        }
 
-                              url_params <- psychTestR::get_url_params(state)
+      })
 
-                              if(length(url_params$dev_vs_prod) == 0L || url_params$dev_vs_prod == "dev") {
-                                url <- "https://singpause.dev.songbird.training"
-                              } else {
-                                url <- "https://singpause.songbird.training"
-                              }
+      )
+    },
 
-                              musicassessr::redirect_page(text = "Vielen Dank für deine Teilnahme! Du wirst jetzt zur App weitergeleitet.",
-                                                          url = url,
-                                                          ms = 2000)
+    final_page = psychTestR::reactive_page(function(state, ...) {
 
-                            })
+      url_params <- psychTestR::get_url_params(state)
+
+      if(length(url_params$dev_vs_prod) == 0L || url_params$dev_vs_prod == "dev") {
+        url <- "https://singpause.dev.songbird.training"
+      } else {
+        url <- "https://singpause.songbird.training"
+      }
+
+      musicassessr::redirect_page(text = "Vielen Dank für deine Teilnahme! Du wirst jetzt zur App weitergeleitet.",
+                                  url = url,ms = 2000)
 
 
-                            )
+    })
 
   )
+
 }
